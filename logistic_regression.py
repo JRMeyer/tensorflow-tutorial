ba72d35a6da1b0bca9f658205d2dee091f8dba3c
michaelcapizzi <mcapizzi@email.arizona.edu>
2016-01-27 13:41:24 -0700

    ready to do ops

--- logistic_regression.py
+++ logistic_regression.py
@@ -2,6 +2,7 @@ import numpy as np
 import tensorflow as tf
 import tarfile
 import os
+import math
 
 #import data
 def untar(tarPath):
@@ -26,29 +27,47 @@ testX = importData("data/testX.csv", delimiter="\t")
 textY = importData("data/testY.csv", delimiter="\t")
 
 #set global parameters
-numFeatures = trainX.shape[1]
-numLabels = trainY.shape[1]
 #number of times we iterate through training data
 epochs = 5
 #batchSize of n
-batchSize = len(trainX)
+batchSize = trainX.shape[0]
 #used by gradientOptimizer
 learningRate = tf.train.exponential_decay(learning_rate=0.01,
                                           global_step= 1,
-                                          decay_steps=len(trainX),
+                                          decay_steps=trainX.shape[0],
                                           decay_rate= 0.95,
                                           staircase=True)
 
-# Define our tensor to hold email data. 16384 = pixels, None indicates
+#determining required dimensions
+numFeatures = trainX.shape[1]
+numLabels = trainY.shape[1]
+
+#set tensorflow placeholders
+    #PUT EXPLANATION HERE
+# Define our tensor to hold email data. None indicates
 # we can hold any number of emails
 x = tf.placeholder(tf.float32, [None, numFeatures])
-# Create the Variables for the weights and biases
-W = tf.Variable(tf.zeros([numFeatures, numLabels]))
-b = tf.Variable(tf.zeros([numLabels]))
-# This is our prediction output
-y = tf.nn.softmax(tf.matmul(x, W) + b)
 # This will be our correct answers/output
-y_ = tf.placeholder(tf.float32, [None, numLabels])
+yGold = tf.placeholder(tf.float32, [None, numLabels])
+
+
+# Create the Variables for the weights and biases
+weights = tf.Variable(tf.random_normal(
+                                        [numFeatures, numLabels],
+                                        mean=0,
+                                        stddev=math.sqrt(float(6) / float(numFeatures + numLabels + 1))),
+                                        name="weights")
+
+biases = tf.Variable(tf.random_normal(
+                                        [1, numLabels],
+                                        mean=0,
+                                        stddev=math.sqrt(float(6) / float(numFeatures + numLabels + 1))),
+                                        name="biases")
+
+# This is our prediction output
+#TODO here
+y = tf.nn.softmax(tf.matmul(x, W) + b)
+
 # Calculate cross entropy by taking the negative sum of our correct values
 # multiplied by the log of our predictions
 cross_entropy = -tf.reduce_sum(y_*tf.log(y))
@@ -57,8 +76,4 @@ train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
 # Initializes all the variables we made above
 init = tf.initialize_all_variables()
 
-
-#set tensorflow placeholders
-# input = tf.placeholder(dtype="float", name="Input", shape=[None, ])
-# label = tf.placeholder(dtype="float", name="Label", shape=[None, ])
 
