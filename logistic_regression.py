8d2441c506cc18df8b7b0fc590542b96a2342fe5
michaelcapizzi <mcapizzi@email.arizona.edu>
2016-01-27 13:22:19 -0700

    modified code to untar and load

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,16 +1,29 @@
 import numpy as np
 import tensorflow as tf
+import tarfile
+import os
 
 #import data
+def untar(tarPath):
+    tarObject = tarfile.open(tarPath)
+    tarObject.extractall()
+    tarObject.close()
+    print "Extracted tar to current directory"
+
 def importData(filePath, delimiter):
     return np.genfromtxt(filePath, delimiter=delimiter, dtype=None)
 
+#untar data file
+if "data" in os.listdir(os.getcwd()):
+    untar("data.tar.gz")
 
 #imports data with following dimensions
-trainX = importData("/data/trainX", delimiter="\t")
-trainY = importData("/data/trainY", delimiter="\t")
-testX = importData("/data/testX", delimiter="\t")
-textY = importData("/data/testY", delimiter="\t")
+print "loading training data"
+trainX = importData("data/trainX.csv", delimiter="\t")
+trainY = importData("data/trainY.csv", delimiter="\t")
+print "loading test data"
+testX = importData("data/testX.csv", delimiter="\t")
+textY = importData("data/testY.csv", delimiter="\t")
 
 #set global parameters
 epochs = 5                                                              #number of times we iterate through training data
@@ -24,5 +37,5 @@ learningRate = tf.train.exponential_decay(                              #used by
 
 
 #set tensorflow placeholders
-input = tf.placeholder(dtype="float", name="Input", shape=[None, ])
-label = tf.placeholder(dtype="float", name="Label", shape=[None, ])
+# input = tf.placeholder(dtype="float", name="Input", shape=[None, ])
+# label = tf.placeholder(dtype="float", name="Label", shape=[None, ])
