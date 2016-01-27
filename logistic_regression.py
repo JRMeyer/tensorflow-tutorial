cd97a778e4fb3cbf724889b229a1abce0f6e5d73
JRMeyer <joshua.richard.meyer@gmail.com>
2016-01-27 13:22:46 -0700

    made some changes

--- logistic_regression.py
+++ logistic_regression.py
@@ -13,14 +13,36 @@ testX = importData("/data/testX", delimiter="\t")
 textY = importData("/data/testY", delimiter="\t")
 
 #set global parameters
-epochs = 5                                                              #number of times we iterate through training data
-batchSize = len(trainX)                                                 #batchSize of n
-learningRate = tf.train.exponential_decay(                              #used by gradientOptimizer
-                                            learning_rate=0.01,
-                                            global_step= 1,
-                                            decay_steps=len(trainX),
-                                            decay_rate= 0.95,
-                                            staircase=True)
+numFeatures = trainX.shape[1]
+numLabels = trainY.shape[1]
+#number of times we iterate through training data
+epochs = 5
+#batchSize of n
+batchSize = len(trainX)
+#used by gradientOptimizer
+learningRate = tf.train.exponential_decay(learning_rate=0.01,
+                                          global_step= 1,
+                                          decay_steps=len(trainX),
+                                          decay_rate= 0.95,
+                                          staircase=True)
+
+# Define our tensor to hold email data. 16384 = pixels, None indicates
+# we can hold any number of emails
+x = tf.placeholder(tf.float32, [None, numFeatures])
+# Create the Variables for the weights and biases
+W = tf.Variable(tf.zeros([numFeatures, numLabels]))
+b = tf.Variable(tf.zeros([numLabels]))
+# This is our prediction output
+y = tf.nn.softmax(tf.matmul(x, W) + b)
+# This will be our correct answers/output
+y_ = tf.placeholder(tf.float32, [None, numLabels])
+# Calculate cross entropy by taking the negative sum of our correct values
+# multiplied by the log of our predictions
+cross_entropy = -tf.reduce_sum(y_*tf.log(y))
+# This is training the NN with backpropagation
+train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
+# Initializes all the variables we made above
+init = tf.initialize_all_variables()
 
 
 #set tensorflow placeholders
